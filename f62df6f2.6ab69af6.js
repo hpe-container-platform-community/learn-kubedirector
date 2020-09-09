611d22c281b5fd500d809cac3be33dede02645b6
Chris Snow <chsnow123@gmail.com>
2020-09-09 07:38:14 +0100

    Deploy website - based on aa2c77f58479e10a168fc14cb878a1ad940dda32

---
+++ f62df6f2.6ab69af6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{86:function(o){o.exports=JSON.parse('{"allTagsPath":"/learn-kubedirector/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/learn-kubedirector/blog/tags/facebook"}')}}]);
