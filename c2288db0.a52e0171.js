7e711eabedf18b2102aced5397784b66e10addee
Chris Snow <chsnow123@gmail.com>
2020-09-09 09:47:39 +0100

    Deploy website - based on e1b2d65d95f5a60ba61c44fe7526c417cf49b1a5

---
+++ c2288db0.a52e0171.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{77:function(e){e.exports=JSON.parse('{"allTagsPath":"/learn-kubedirector/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/learn-kubedirector/blog/tags/hello"}')}}]);
