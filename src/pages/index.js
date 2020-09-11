dafe3fd796072e4235a111565db0994dbba40dfe
Chris Snow <chsnow123@gmail.com>
2020-09-11 15:27:26 +0100

    more content

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Education resources for learning KubeDirector <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
