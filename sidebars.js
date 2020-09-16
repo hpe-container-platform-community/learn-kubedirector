0a6003022294a3179407b8f520d5b12d149d87a5
Chris Snow <chsnow123@gmail.com>
2020-09-16 18:58:41 +0100

    more docs

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Introduction: ['intro', 'prereqs' ],
-    "Lab Installation": ['lab/overview', 'lab/install1', 'lab/install2', 'lab/install3'],
+    "Lab Installation": ['lab/overview', 'lab/install1', 'lab/install2', 'lab/install3', 'lab/advanced'],
     "Using KD": ['kd-user/intro', 'kd-user/concepts', 'kd-user/using', 'kd-user/storage', 'kd-user/configuring', 'kd-user/ref'],
     "KD Image Development": ['kd-img-dev/overview'],
     "KD Development": ['kd-dev/overview'],
